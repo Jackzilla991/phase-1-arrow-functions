6fc6ca588384b322c479b89569900681914d02ed
Jack Scarpelli <Festivetomcat99@gmail.com>
2022-12-15 22:14:07 -0500

    commit tree 4e7ed415222647545399dd361f88c396a64416f3

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+
+// function expression to divide two numbers
+const divide = (x, y) => {
+    return x / y;
+  }
+  
+  // arrow function to square a number
+  const square = x => x * x;
+  
+  // arrow function to add two numbers
+  const add = (x, y) => x + y;
+  
